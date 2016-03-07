1301765838c72d03e869d4c5b1326f4cb8f23dde
Jordan Lovato <jordanlovato@gmail.com>
2016-03-07 11:28:32 -0800

    added back autoload.php

---
+++ vendor/autoload.php
@@ -0,0 +1,9 @@
+<?php
+
+//function fedex_api_autoload($class) {
+//    error_log(LIB_ROOT. DIRECTORY_SEPARATOR . 'php-fedex-api-wrapper' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $class . '.php');
+//    if (file_exists(LIB_ROOT. DIRECTORY_SEPARATOR . 'php-fedex-api-wrapper' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $class . '.php'))
+//        include_once(LIB_ROOT. DIRECTORY_SEPARATOR . 'php-fedex-api-wrapper' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $class . '.php');
+//}
+//
+//spl_autoload_register('fedex_api_autoload');
